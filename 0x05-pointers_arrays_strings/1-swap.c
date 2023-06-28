ac2ee4001c91ccab1ffb11bb3e8bd954d5098fc5
Your Name <you@example.com>
2023-06-27 17:21:35 -0700

    arrays and pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,15 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#include "main.h"
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first value
+ * @b: pointer to second value
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
 
-#endif
+	c = *a;
+	*a = *b;
+	*b = c;
+}
