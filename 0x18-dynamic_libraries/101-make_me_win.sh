096188958608cece45ebba603eef346de6c55e80
Imane Benammou <114099911+Ibenammou@users.noreply.github.com>
2023-10-21 08:12:03 +0100

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Load the LD_PRELOAD environment variable to inject our shared library
-
-export LD_PRELOAD="./winning.so"
-
-# Run the Giga Millions program with the desired numbers
-./gm 9 8 10 24 75 9
-
